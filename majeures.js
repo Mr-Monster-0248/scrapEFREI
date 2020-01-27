f8ab595293182e24ea1bf2774875b4cabbd2b517
Thibault Lepez <thibault.lepez@efrei.net>
2020-01-27 18:13:59 +0100

    made a function out of majeur.js

--- majeures.js
+++ majeures.js
@@ -1,26 +1,32 @@
 const request = require('request');
 const cheerio = require('cheerio');
-const url = 'https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/';
-tab = []
 
-request(url, (error, response, html) => {
-    if(!error && response.statusCode == 200) {
+
+function getMajeures(){
+    const url ="https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/";
+    tab = [];
+    request(url, (error, response, html) => {
+      if (!error && response.statusCode == 200) {
         const $ = cheerio.load(html);
+        
+        test = $(".element-anime").each( (i, elem) => {
+          tab[i] = {
+            img: "",
+            descript: "",
+            title: "",
+            link: ""
+          };
 
-        test = $('.element-anime').each(function(i, elem) {
-            tab[i] = {
-                img: '',
-                descript: '',
-                title:'',
-                link:''   
-            };
-            tab[i].title = $(this).find('h3').text();
-            tab[i].descript = $(this).find('p').text();
-            text = $(this).find('.imgbg').attr('style');
-            tab[i].img = text.substring(16, text.length-3);
-            tab[i].link = $(this).find('a').attr('href');
-        })
+          tab[i].title = $(elem).find("h3").text();
+          tab[i].descript = $(elem).find("p").text();
+          text = $(elem).find(".imgbg").attr("style");
+          tab[i].img = text.substring(16, text.length - 3);
+          tab[i].link = $(elem).find("a").attr("href");
+        });
+      }
+    });
+    return tab;
+}
 
-        return tab;  
-    }
-});
+test = getMajeures();
+console.log(test);
