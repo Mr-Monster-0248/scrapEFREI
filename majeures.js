07189871801ff94cb4315a4cea08763a76dcae60
Thibault Lepez <thibault.lepez@efrei.net>
2020-01-28 23:35:47 +0100

    Solved majeur.js

--- majeures.js
+++ majeures.js
@@ -2,12 +2,14 @@ const request = require('request');
 const cheerio = require('cheerio');
 
 
-function getMajeures(){
-    const url ="https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/";
+async function getMajeures(){
+	return new Promise( (resolve, reject) => {
+		const url ="https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/";
     tab = [];
     request(url, (error, response, html) => {
       if (!error && response.statusCode == 200) {
-        const $ = cheerio.load(html);
+				const $ = cheerio.load(html);
+			
         
         test = $(".element-anime").each( (i, elem) => {
           tab[i] = {
@@ -17,16 +19,27 @@ function getMajeures(){
             link: ""
           };
 
-          tab[i].title = $(elem).find("h3").text();
+          tab[i].title = $(elem).find("h3").text().replace("Ggrids", "Grids");
           tab[i].descript = $(elem).find("p").text();
           text = $(elem).find(".imgbg").attr("style");
           tab[i].img = text.substring(16, text.length - 3);
           tab[i].link = $(elem).find("a").attr("href");
         });
-      }
-    });
-    return tab;
+			} else {
+				reject("Could not resolve url");
+			}
+
+      resolve(tab);
+		});
+	})
 }
 
-test = getMajeures();
-console.log(test);
+
+(async function() {
+	try { 
+		let data = await getMajeures();
+  	console.log("Success:", data);
+	} catch (error) {
+		console.log("Error:", error);
+	}  
+})();
